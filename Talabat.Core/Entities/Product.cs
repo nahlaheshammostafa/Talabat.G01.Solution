4acf70b772a0f9918048c55c291fb9d18f7606b8
Nahla <nahlahesham300@gmail.com>
2024-04-09 06:20:05 +0200

    Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	internal class Product : BaseEntity
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string PictureUrl { get; set; }
+		public decimal Price { get; set; }
+
+		public int BrandId { get; set; } //Foregin Key Column => ProductBrand
+		public ProductBrand Brand { get; set; } // Navigational Property [ONE]
+
+		public int CategoryId { get; set; }  //Foregin Key Column => ProductCategory
+		public ProductCategory Category { get; set; } // Navigational Property [ONE]
+
+	}
+}
