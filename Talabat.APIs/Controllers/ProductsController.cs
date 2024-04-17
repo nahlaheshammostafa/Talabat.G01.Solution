969ac0554bda064b2d76d47135182b483e8f5c55
Nahla <nahlahesham300@gmail.com>
2024-04-17 15:40:51 +0200

    Product DTO Using AutoMapper

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.Dtos;
 using Talabat.Core.Entities;
 using Talabat.Core.Repositories.Contract;
 using Talabat.Core.Specifications.Product_Specs;
@@ -9,27 +11,30 @@ namespace Talabat.APIs.Controllers
 	public class ProductsController : BaseApiController
 	{
 		private readonly IGenericRepository<Product> _productsRepo;
-		public ProductsController(IGenericRepository<Product> productsRepo)
+		private readonly IMapper _mapper;
+
+		public ProductsController(IGenericRepository<Product> productsRepo, IMapper mapper)
 		{
 			_productsRepo = productsRepo;
+			_mapper = mapper;
 		}
 		//  /api/Products
 		[HttpGet]
-		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+		public async Task<ActionResult<IEnumerable<ProductToReturnDto>>> GetProducts()
 		{
 			var spec = new ProductWithBrandAndCategorySpecifications();
 			var products = await _productsRepo.GetAllWithSpecAsync(spec);
-			return Ok(products);
+			return Ok(_mapper.Map<IEnumerable<Product>,IEnumerable<ProductToReturnDto>>(products));
 		}
 
 		[HttpGet("{id}")]
-		public async Task<ActionResult<Product>> GetProduct(int id)
+		public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
 		{
 			var spec = new ProductWithBrandAndCategorySpecifications(id);
 			var product = await _productsRepo.GetWithSpecAsync(spec);
 			if(product is null)
 				return NotFound(new { Message = "Not Found", StatusCode = 404 }); //404
-			return Ok(product); //200
+			return Ok(_mapper.Map<Product,ProductToReturnDto>(product)); //200
 		}
 
 	}
---
+++ Talabat.APIs/Helpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Talabat.APIs.Dtos;
+using Talabat.Core.Entities;
+
+namespace Talabat.APIs.Helpers
+{
+	public class MappingProfiles : Profile
+	{
+		public MappingProfiles() 
+		{
+			CreateMap<Product,ProductToReturnDto>()
+				.ForMember(d => d.Brand, O => O.MapFrom(S => S.Brand.Name))
+				.ForMember(d => d.Category, O => O.MapFrom(S => S.Category.Name));
+		}
+	}
+}
