06ac4ce63dc45e77dab524ec8c23e2102f7d1844
Nahla <nahlahesham300@gmail.com>
2024-04-24 11:27:42 +0200

    Basket Controller

---
+++ Talabat.APIs/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.Errors;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+
+namespace Talabat.APIs.Controllers
+{
+	public class BasketController : BaseApiController
+	{
+		private readonly IBasketRepository _basketRepository;
+
+		public BasketController(IBasketRepository basketRepository) 
+		{
+			_basketRepository = basketRepository;
+		}
+
+		[HttpGet]  //GET: / api/basket?id=
+		public async Task<ActionResult<CustomerBasket>> GetBasket(string id)
+		{
+			var basket = await _basketRepository.GetBasketAsync(id);
+			return Ok(basket ?? new CustomerBasket(id));
+		}
+
+		[HttpPost]  //POST: /api/basket
+		public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+		{
+			var createOrUpdateBasket = await _basketRepository.UpdateBasketAsync(basket);
+			if (createOrUpdateBasket is null) return BadRequest(new ApiResponse(400));
+			return Ok(createOrUpdateBasket);
+		}
+
+		[HttpDelete] //DELETE: /api/basket?id=
+		public async Task DeleteBasket(string id)
+		{
+			await _basketRepository.DeleteBasketAsync(id);
+		}
+	}
+}
--- Talabat.Core/Entities/CustomerBasket.cs
+++ Talabat.Core/Entities/CustomerBasket.cs
@@ -10,6 +10,11 @@ namespace Talabat.Core.Entities
 	{
 		public string Id { get; set; }
 		public List<BasketItem> Items { get; set; }
+		public CustomerBasket(string id) 
+		{
+			Id = id;
+			Items = new List<BasketItem>();
+		}
 
 	}
 }
